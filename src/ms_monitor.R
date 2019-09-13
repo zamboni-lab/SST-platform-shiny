8cbe8594aa1aa688b697d568d087f5a3401ea8f6
dmitrav <dmitrav@inbox.ru>
2019-09-13 13:34:30 +0200

    v.0.0.5: tab and table added

--- src/ms_monitor.R
+++ src/ms_monitor.R
@@ -18,66 +18,71 @@ qc_metrics_descriptions = data.frame(names=colnames(qc_values)[-1], descriptions
 # Define UI
 ui <- fluidPage(
   
-    # Give the page a title
-    titlePanel("QC characteristics"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        selectInput("metric", "Choose a QC characteristic:", 
-                    choices=colnames(qc_values)[-1]),
-        hr(),
-        helpText("All acquired data since 2019-05-24 is shown."),
-        hr(),
-        htmlOutput("metric_description")
-      ),
-      
-        
-      
-      # Create a spot for the barplot
-      mainPanel(
-        plotOutput("distribution_plot"),
-        plotOutput("chonological_plot")
-      )
-    )
+  # Give the page a title
+  titlePanel("QC characteristics"),
+  
+  tabsetPanel(type = "tabs",
+              tabPanel("Plots", 
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("metric", "Choose a QC characteristic:", 
+                                       choices=colnames(qc_values)[-1]),
+                           hr(),
+                           helpText("All acquired data since 2019-05-24 is shown."),
+                           hr(),
+                           htmlOutput("metric_description")
+                         ),
+                         mainPanel(
+                           plotOutput("distribution_plot"),
+                           plotOutput("chonological_plot")
+                         )
+                       )),
+              tabPanel("Table",
+                       tableOutput("table"))
+  )
 )
+
+# Generate a row with a sidebar
 
 # Define server logic
 server <- function(input, output) {
+  
+  # Fill in the spot we created for a plot
+  output$distribution_plot = renderPlot({
     
-    # Fill in the spot we created for a plot
-    output$distribution_plot = renderPlot({
-      
-      ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) + 
-        geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 50) +
-        geom_density(alpha=.3, fill="lightblue") +
-        geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ), 
-                   linetype = "dashed", size = 0.8, color = "#FC4E07") +
-        labs(x = "Value", y = "Frequency") + 
-        ggtitle(input$metric) +
-        theme(plot.title = element_text(hjust = 0.5))
-
-        })
+    ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) + 
+      geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 50) +
+      geom_density(alpha=.3, fill="lightblue") +
+      geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ), 
+                 linetype = "dashed", size = 0.8, color = "#FC4E07") +
+      labs(x = "Value", y = "Frequency") + 
+      ggtitle(input$metric) +
+      theme(plot.title = element_text(hjust = 0.5))
     
-    output$chonological_plot = renderPlot({
-      
-      ggplot(qc_values, aes(x = acquisition_date, y = eval(parse(text=input$metric)))) +
-        geom_point(size = 2) + geom_line(group = 1) +
-        geom_point(data=qc_values[nrow(qc_values), c(input$metric, "acquisition_date")], aes(x = acquisition_date, y = eval(parse(text=input$metric))), color="red", size=2) +  # add red dot in the end
-        theme(axis.text.x = element_text(angle = 90)) + 
-        labs(x = "Date & time", y = "Value") + 
-        ggtitle(input$metric) +
-        theme(plot.title = element_text(hjust = 0.5))
-      
-    })
+  })
+  
+  output$chonological_plot = renderPlot({
     
-    output$metric_description = renderUI({
-      HTML(paste(paste(input$metric, "is computed as"), qc_metrics_descriptions[qc_metrics_descriptions$names == input$metric, "descriptions"], sep="<br/>"))
-      
-    })
+    ggplot(qc_values, aes(x = acquisition_date, y = eval(parse(text=input$metric)))) +
+      geom_point(size = 2) + geom_line(group = 1) +
+      geom_point(data=qc_values[nrow(qc_values), c(input$metric, "acquisition_date")], aes(x = acquisition_date, y = eval(parse(text=input$metric))), color="red", size=2) +  # add red dot in the end
+      theme(axis.text.x = element_text(angle = 90)) + 
+      labs(x = "Date & time", y = "Value") + 
+      ggtitle(input$metric) +
+      theme(plot.title = element_text(hjust = 0.5))
     
+  })
+  
+  output$table <- renderTable({ qc_values },
+                              hover = TRUE, bordered = TRUE,
+                              spacing = 'xs', width = "auto", align = 'c',
+                              digits = 4)
+  
+  output$metric_description = renderUI({
+    HTML(paste(paste(input$metric, "is computed as"), qc_metrics_descriptions[qc_metrics_descriptions$names == input$metric, "descriptions"], sep="<br/>"))
+    
+  })
+  
 }
 
 # Run the app
--- src/tries.R
+++ src/tries.R
@@ -1,120 +1,93 @@
 
-
-library(RSQLite)
-library(DBI)
+library(shiny)
 library(ggplot2)
+library(DBI)
+library(RSQLite)
+
+source("constants.R")
 
 # connect to the sqlite file
-con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/qc_metrics/res/nas2/qc_matrix.db")
+# con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/qc_matrix_example.db")
+con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/nas2_qc_matrix_sep10.db")
 
-# get a list of all tables
-alltables = dbListTables(con)
 
-# get the data as a data.frame
-p1 = dbGetQuery(con, 'select * from qc_values')
-p2 = dbGetQuery(con, 'select * from qc_meta')
-
-p1$date = unlist(lapply(p2$original_filename, function(x) substring(x, 1, 10) )) 
-p1$date = p2$original_filename
-
-colnames(p1)
-
-ggplot(data = p1, aes(x = date, y = resolution_200)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = resolution_700)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = average_accuracy)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = chemical_dirt)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = instrument_noise)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = isotopic_presence)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = transmission)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = fragmentation_305)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = fragmentation_712)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = baseline_25_150)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = baseline_50_150)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = baseline_25_650)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = baseline_50_650)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = signal)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = s2b)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(data = p1, aes(x = date, y = s2n)) +
-  geom_point(size = 2) + geom_line(group = 1) +
-  theme(axis.text.x = element_text(angle = 90))
+# get qc_values as a dataframe
+qc_values = dbGetQuery(con, 'select * from qc_values')
+qc_metrics_descriptions = data.frame(names=colnames(qc_values)[-1], descriptions=descriptions)
 
 
 
 
 
+# Define UI
+ui <- fluidPage(
+  
+  # Give the page a title
+  titlePanel("QC characteristics"),
+  
+  tabsetPanel(type = "tabs",
+              tabPanel("Plots", 
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("metric", "Choose a QC characteristic:", 
+                                       choices=colnames(qc_values)[-1]),
+                           hr(),
+                           helpText("All acquired data since 2019-05-24 is shown."),
+                           hr(),
+                           htmlOutput("metric_description")
+                        ),
+                         mainPanel(
+                           plotOutput("distribution_plot"),
+                           plotOutput("chonological_plot")
+                         )
+                       )),
+               tabPanel("Table",
+                        tableOutput("table"))
+  )
+)
+  
+  # Generate a row with a sidebar
+ 
+# Define server logic
+server <- function(input, output) {
+  
+  # Fill in the spot we created for a plot
+  output$distribution_plot = renderPlot({
+    
+    ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) + 
+      geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 50) +
+      geom_density(alpha=.3, fill="lightblue") +
+      geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ), 
+                 linetype = "dashed", size = 0.8, color = "#FC4E07") +
+      labs(x = "Value", y = "Frequency") + 
+      ggtitle(input$metric) +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
+  output$chonological_plot = renderPlot({
+    
+    ggplot(qc_values, aes(x = acquisition_date, y = eval(parse(text=input$metric)))) +
+      geom_point(size = 2) + geom_line(group = 1) +
+      geom_point(data=qc_values[nrow(qc_values), c(input$metric, "acquisition_date")], aes(x = acquisition_date, y = eval(parse(text=input$metric))), color="red", size=2) +  # add red dot in the end
+      theme(axis.text.x = element_text(angle = 90)) + 
+      labs(x = "Date & time", y = "Value") + 
+      ggtitle(input$metric) +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
+  output$table <- renderTable({ qc_values },
+                              hover = TRUE, bordered = TRUE,
+                              spacing = 'xs', width = "auto", align = 'c',
+                              digits = 4)
+  
+  output$metric_description = renderUI({
+    HTML(paste(paste(input$metric, "is computed as"), qc_metrics_descriptions[qc_metrics_descriptions$names == input$metric, "descriptions"], sep="<br/>"))
+    
+  })
+  
+}
 
-
-
-
-
-
-p1[,-1]
-
-barplot(p1[,"resolution_200"])
-
-
-# Draw with black outline, white fill
-ggplot(p1, aes(x=resolution_200)) +
-  geom_density(alpha=.4, fill="lightblue") +
-  labs(x = "values", y = "frequency")
-
-# + geom_vline(aes(xintercept = mean(weight)), 
-#              linetype = "dashed", size = 0.6,
-#              color = "#FC4E07")
-
-ggplot(p1, aes(x=resolution_200)) + geom_histogram(binwidth=.5)
-
-# Density curve
-ggplot(dat, aes(x=rating)) + geom_density()
-
-# Histogram overlaid with kernel density curve
-ggplot(dat, aes(x=rating)) + 
-  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
-                 binwidth=.5,
-                 colour="black", fill="white") +
-  geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+# Run the app
+shinyApp(ui = ui, server = server)
