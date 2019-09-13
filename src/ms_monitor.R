2bf839aff9e750d1f0a29157f5f59b4fc5c7e498
dmitrav <dmitrav@inbox.ru>
2019-09-13 17:18:30 +0200

    v.0.0.6: table style updated + qc score added

--- src/ms_monitor.R
+++ src/ms_monitor.R
@@ -7,7 +7,6 @@ library(RSQLite)
 source("constants.R")
 
 # connect to the sqlite file
-# con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/qc_matrix_example.db")
 con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/nas2_qc_matrix_sep10.db")
 
 
@@ -16,16 +15,16 @@ qc_values = dbGetQuery(con, 'select * from qc_values')
 qc_metrics_descriptions = data.frame(names=colnames(qc_values)[-1], descriptions=descriptions)
 
 # Define UI
-ui <- fluidPage(
+ui <- shinyUI(fluidPage(
   
   # Give the page a title
   titlePanel("QC characteristics"),
   
   tabsetPanel(type = "tabs",
-              tabPanel("Plots", 
+              tabPanel("Plots",
                        sidebarLayout(
                          sidebarPanel(
-                           selectInput("metric", "Choose a QC characteristic:", 
+                           selectInput("metric", "Choose a QC characteristic:",
                                        choices=colnames(qc_values)[-1]),
                            hr(),
                            helpText("All acquired data since 2019-05-24 is shown."),
@@ -40,22 +39,49 @@ ui <- fluidPage(
               tabPanel("Table",
                        tableOutput("table"))
   )
-)
+))
 
-# Generate a row with a sidebar
+color.qc.table = function(table){
+  
+  scoring = table
+  
+  for (i in 2:ncol(table)){
+    
+    values = table[,i][table[,i] > 0]
+    
+    q25 = quantile(values, .25)
+    q75 = quantile(values, .75)
+    
+    scoring[,i] = ifelse (table[,i] > q25 & table[,i] < q75, 1, 0)
+    
+    table[,i] = paste(
+      '<div style="background-color: ',
+      ifelse (table[,i] > q25 & table[,i] < q75, "#AAFF8A", "#FF968D"),
+      '; border-radius: 5px;">',
+      round(table[,i], 4),
+      '</div>',
+      sep=''
+    )
+  }
+  
+  table[,1] = substring(table[,1], 1, 10)
+  table$score = paste(apply(scoring[,-1], 1, sum), "/", ncol(scoring)-1, sep = "")
+  
+  return(table)
+}
 
 # Define server logic
-server <- function(input, output) {
+server <- shinyServer(function(input, output) {
   
   # Fill in the spot we created for a plot
   output$distribution_plot = renderPlot({
     
-    ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) + 
+    ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) +
       geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 50) +
       geom_density(alpha=.3, fill="lightblue") +
-      geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ), 
+      geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ),
                  linetype = "dashed", size = 0.8, color = "#FC4E07") +
-      labs(x = "Value", y = "Frequency") + 
+      labs(x = "Value", y = "Frequency") +
       ggtitle(input$metric) +
       theme(plot.title = element_text(hjust = 0.5))
     
@@ -66,24 +92,25 @@ server <- function(input, output) {
     ggplot(qc_values, aes(x = acquisition_date, y = eval(parse(text=input$metric)))) +
       geom_point(size = 2) + geom_line(group = 1) +
       geom_point(data=qc_values[nrow(qc_values), c(input$metric, "acquisition_date")], aes(x = acquisition_date, y = eval(parse(text=input$metric))), color="red", size=2) +  # add red dot in the end
-      theme(axis.text.x = element_text(angle = 90)) + 
-      labs(x = "Date & time", y = "Value") + 
+      theme(axis.text.x = element_text(angle = 90)) +
+      labs(x = "Date & time", y = "Value") +
       ggtitle(input$metric) +
       theme(plot.title = element_text(hjust = 0.5))
     
   })
   
-  output$table <- renderTable({ qc_values },
+  colored_values = color.qc.table(qc_values)
+  
+  output$table <- renderTable({ colored_values },
                               hover = TRUE, bordered = TRUE,
                               spacing = 'xs', width = "auto", align = 'c',
-                              digits = 4)
+                              sanitize.text.function = function(x) x)
   
   output$metric_description = renderUI({
     HTML(paste(paste(input$metric, "is computed as"), qc_metrics_descriptions[qc_metrics_descriptions$names == input$metric, "descriptions"], sep="<br/>"))
     
   })
-  
-}
+})
 
 # Run the app
 shinyApp(ui = ui, server = server)
--- src/tries.R
+++ src/tries.R
@@ -1,93 +1,25 @@
 
+# "#EC6D57" "#81D652"
+
+
 library(shiny)
-library(ggplot2)
-library(DBI)
-library(RSQLite)
 
-source("constants.R")
-
-# connect to the sqlite file
-# con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/qc_matrix_example.db")
-con = dbConnect(SQLite(), dbname="/Users/andreidm/ETH/projects/ms_monitor/data/nas2_qc_matrix_sep10.db")
-
-
-# get qc_values as a dataframe
-qc_values = dbGetQuery(con, 'select * from qc_values')
-qc_metrics_descriptions = data.frame(names=colnames(qc_values)[-1], descriptions=descriptions)
-
-
-
-
-
-# Define UI
-ui <- fluidPage(
-  
-  # Give the page a title
-  titlePanel("QC characteristics"),
-  
-  tabsetPanel(type = "tabs",
-              tabPanel("Plots", 
-                       sidebarLayout(
-                         sidebarPanel(
-                           selectInput("metric", "Choose a QC characteristic:", 
-                                       choices=colnames(qc_values)[-1]),
-                           hr(),
-                           helpText("All acquired data since 2019-05-24 is shown."),
-                           hr(),
-                           htmlOutput("metric_description")
-                        ),
-                         mainPanel(
-                           plotOutput("distribution_plot"),
-                           plotOutput("chonological_plot")
-                         )
-                       )),
-               tabPanel("Table",
-                        tableOutput("table"))
-  )
+ui <- shinyUI(fluidPage(
+  tableOutput("tt"))
 )
-  
-  # Generate a row with a sidebar
- 
-# Define server logic
-server <- function(input, output) {
-  
-  # Fill in the spot we created for a plot
-  output$distribution_plot = renderPlot({
-    
-    ggplot(qc_values, aes(x=eval(parse(text=input$metric)))) + 
-      geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 50) +
-      geom_density(alpha=.3, fill="lightblue") +
-      geom_vline(aes(xintercept = tail(qc_values[,input$metric], n=1) ), 
-                 linetype = "dashed", size = 0.8, color = "#FC4E07") +
-      labs(x = "Value", y = "Frequency") + 
-      ggtitle(input$metric) +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-  })
-  
-  output$chonological_plot = renderPlot({
-    
-    ggplot(qc_values, aes(x = acquisition_date, y = eval(parse(text=input$metric)))) +
-      geom_point(size = 2) + geom_line(group = 1) +
-      geom_point(data=qc_values[nrow(qc_values), c(input$metric, "acquisition_date")], aes(x = acquisition_date, y = eval(parse(text=input$metric))), color="red", size=2) +  # add red dot in the end
-      theme(axis.text.x = element_text(angle = 90)) + 
-      labs(x = "Date & time", y = "Value") + 
-      ggtitle(input$metric) +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-  })
-  
-  output$table <- renderTable({ qc_values },
-                              hover = TRUE, bordered = TRUE,
-                              spacing = 'xs', width = "auto", align = 'c',
-                              digits = 4)
-  
-  output$metric_description = renderUI({
-    HTML(paste(paste(input$metric, "is computed as"), qc_metrics_descriptions[qc_metrics_descriptions$names == input$metric, "descriptions"], sep="<br/>"))
-    
-  })
-  
-}
 
-# Run the app
+qc_values[6,2]
+
+server <- shinyServer(function(input, output) {
+
+  test[1, 1] = '<div style="background-color: #EC6D57; border-radius: 5px;">1</div>'
+  test[1, 2] = "asfsf"
+  test[2, 2] = '<div style="background-color: #81D652; border-radius: 5px;">2</div>'
+
+
+  output$tt <- renderTable({
+    test
+  }, sanitize.text.function = function(x) x)
+})
+
 shinyApp(ui = ui, server = server)
